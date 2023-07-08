a5938d312eadc10a28313fcad2ef46532e8ff24f
Venkatesalu-b <103803055+Venkatesalu-b@users.noreply.github.com>
2023-07-08 20:38:44 +0530

    Add files via upload

---
+++ Images.js
@@ -0,0 +1,6 @@
+export default{
+    Biketracking_Loginimage: require('../Assets/wifi.webp'),
+    Biketracking_Emblemimage: require('../Assets/Indianemblem.png'),
+    Biketracking_Bikeimage: require('../Assets/rebike.jpg'),
+    
+}
